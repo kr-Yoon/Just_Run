4272f97bad615f0bc120e336956edac7d8f74d34
kr-Yoon <yoon92_kr@naver.com>
2021-08-23 21:56:02 +0900

    2021.08.23 진법
    ▶ 기본적으로 10진법을 이용한다.
    ▶ 필요에 따라 2진법을 이용하고자 한다면 리터럴값 앞에 0b 혹은 0B를 붙인다.
    ▶ 8진법을 이용하고자 한다면 리터럴값 앞에 '0'을 붙인다.
    ▶ 16진법을 이용하고자 한다면 리터럴값 앞에 0x 혹은 0X를 붙인다. 단, 소문자를 사용했을경우에는 10진법 10에 해당하는 값이 16진법에서는 'a'가 되며, 대문자를 사용할 경우에는 'A'가 된다.

---
+++ Code4_6.java
@@ -0,0 +1,18 @@
+package pro01;
+
+public class Code4_6 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 0b1010;
+		int c = 012;
+		int d = 0xa;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+	}
+
+}
