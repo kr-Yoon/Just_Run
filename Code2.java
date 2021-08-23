53ef3a25c02bb5d4b1114f9a5b3ebb1d138ff190
kr-Yoon <yoon92_kr@naver.com>
2021-08-23 21:52:00 +0900

    2021.08.23 boolean
    boolean은 참과 거짓을 참과 거짓을 저장하는 자료형이다.
    ▶ 1byte의 메모리를 할당한다.

---
+++ Code2.java
@@ -0,0 +1,19 @@
+package pro01;
+
+public class Code2 {
+
+	public static void main(String[] args) {
+		boolean x = true;
+		boolean y = false;
+		System.out.println(x);
+		System.out.println(y);
+		
+		x = 5>10;
+		System.out.println(x);
+		
+		y = 5<10;
+		System.out.println(y);
+		
+	}
+
+}
