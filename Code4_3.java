4a495251b7b889d9fdc5e1bb03efece1cc9e607a
kr-Yoon <yoon92_kr@naver.com>
2021-08-23 21:53:30 +0900

    2021.08.23 int
    정수 자료형의 기본형인 int이다.
    ▶ int는 4byte 의 메모리를 할당한다.
    ▶ cpu의 최적의 환경인 4byte이기 때문에 일반적으로 int를 사용한다고 한다.

---
+++ Code4_3.java
@@ -0,0 +1,19 @@
+package pro01;
+
+public class Code4_3 {
+
+	public static void main(String[] args) {
+		int i = 2147483647;
+		System.out.println(i);
+		i++;
+		System.out.println(i);
+		
+		int j = -2147483648;
+		System.out.println(j);
+		j--;
+		System.out.println(j);
+		
+
+	}
+
+}
