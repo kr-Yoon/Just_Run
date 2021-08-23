88a81f6394a7a5ce31263bec4b75b90b6e621c27
kr-Yoon <yoon92_kr@naver.com>
2021-08-23 21:53:20 +0900

    2021.08.23 short
    정수 자료형 중 하나인 short 이다.
    ▶ byte는 2 byte의 메모리를 할당받는다.
    ▶ byte가 표현할 수 있는 수는 -32768 ~ 32767 이다.

---
+++ Code4_2.java
@@ -0,0 +1,23 @@
+package pro01;
+
+public class Code4_2 {
+
+	public static void main(String[] args) {
+		short s = 32767;
+		System.out.println(s);
+		s++;
+		System.out.println(s);
+		s++;
+		System.out.println(s);
+		
+		short v = -32767;
+		System.out.println(v);
+		v--;
+		System.out.println(v);
+		v--;
+		System.out.println(v);
+	
+
+	}
+
+}
