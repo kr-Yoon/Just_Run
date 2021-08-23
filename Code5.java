8852c5286c706e85c59698d7de4d9e7ef186cc23
kr-Yoon <73813647+kr-Yoon@users.noreply.github.com>
2021-08-23 22:11:21 +0900

    Update Code5.java

--- Code5.java
+++ Code5.java
@@ -4,7 +4,7 @@ public class Code5 {
 
 	public static void main(String[] args) {
 		String myName = "Peter Pan";
-		String yourName = "ȫ�浿";
+		String yourName = "Pig";
 		System.out.println("I am" +myName+ ".");
 		System.out.println("You are " +yourName+".");
 	}
