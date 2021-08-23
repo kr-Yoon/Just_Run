8a1b3b6f91aeb04643df268bd5cc291974850b80
kr-Yoon <yoon92_kr@naver.com>
2021-08-23 21:53:05 +0900

    2021.08.23 byte
    정수 자료형 중 하나인 byte 이다.
    ▶ byte는 1 byte의 메모리를 할당받는다.
    ▶ byte가 표현할 수 있는 수는 -128 ~ 127 이다. 1byte에는 8bit의 메모리를 이용 가능하며, 8bit 중 첫 bit는 (0 == 양수, 1 == 음수) 음양을 표현하는데 사용되며 나머지 7bit로 표현할 수 있는 2진법의 범위이다.
    ▶ 0은 양수에 포함된다.
    ▶ 1byte의 저장가능 범위인 127을 초과하는 값은 저장할수 없기에 에러가 난다. 단, 코드를 통해 127이 될 경우 2의 보수에 의해 음의 최소값인 -128이 된다.

---
+++ Code4_1.java
@@ -0,0 +1,24 @@
+package pro01;
+
+public class Code4_1 {
+
+	
+	public static void main(String[] args) {
+		byte b = 126;
+		System.out.println(b);
+		b++;
+		System.out.println(b);
+		b++;
+		System.out.println(b);
+		
+				
+		byte a = -127;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+			
+		
+	}
+}
