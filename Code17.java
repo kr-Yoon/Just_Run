7b9fe1c0a155573351bae4940e5de695fed9c67b
kr-Yoon <yoon92_kr@naver.com>
2021-08-24 21:29:30 +0900

    2021.08.24 조건문
    ▶ if (조건) {참일경우 실행문}
    하위 조건을 추가하고자 한다면 else if, else 를 사용한다.

---
+++ Code17.java
@@ -0,0 +1,42 @@
+package pro02;
+	//조건문의 형태는 if (조건) {참일경우 실행문} 으로 진행된다.
+public class Code17 {
+
+	public static void main(String[] args) {
+		int score1 = 95;
+		if (score1 >= 90) {
+			System.out.println("PASS!!");
+		}
+		System.out.println("ending... program");
+		
+		
+		
+		int score2 = 85;
+		if (score2 >= 90) {
+			System.out.println("PASS!!");
+			}
+		System.out.println("ending... program");
+		
+		
+		
+		int score3 = 95;
+		if (score3 >= 90) {
+			System.out.println("PASS!!");
+			}
+		else {System.out.println("FAIL!!");	
+		}
+		System.out.println("ending...program");
+		
+		
+		
+		int score4 = 85;
+		if (score4 >= 90) {
+			System.out.println("PASS!!");
+		}
+		else {
+			System.out.println("FAIL!!");
+		}
+		System.out.println("ending... program");
+	}
+
+}
