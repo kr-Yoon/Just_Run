2f25d54f812077fb0968470b5c3abe699a681e45
kr-Yoon <yoon92_kr@naver.com>
2021-08-24 21:27:24 +0900

    2021.08.24 캐스터
    캐스터는 자료형을 일시적으로 호출하는 강제 형변환이지만, 내용 자체에는 영향을 미치지 않는다.

---
+++ Code15_2.java
@@ -0,0 +1,22 @@
+package pro02;
+	//캐스트 연산자 보충
+public class Code15_2 {
+
+	public static void main(String[] args) {
+		
+		byte bi, bj;
+		int i = 100, j = 300;
+		bi = (byte)i;	
+		System.out.println("bi : "+bi);
+		bj = (byte)j;  //일시적으로 j의 값을 byte화, byte가 표현할수 있는 값은 -128 ~ 127 이므로 허용범휘로 치환 후 표기
+		System.out.println("bj : "+bj);
+		
+		byte b1 = 10, b2;
+		int a = 5;
+		b2 = (byte)(b1 + a);
+		System.out.println("b2 : "+b2);
+		
+
+	}
+
+}
