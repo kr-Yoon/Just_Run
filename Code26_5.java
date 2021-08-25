d7ede737e73f4993741ffce9358f2cbc896c6c1b
kr-Yoon <yoon92_kr@naver.com>
2021-08-25 20:57:31 +0900

    2021.08.25 배열의 대입
    배열 간의 대입도 가능하다. 단 대입을 하게 되면 같은 참조값을 같는다는 의미이므로 동일한 heap 주소를 사용한다.

---
+++ Code26_5.java
@@ -0,0 +1,22 @@
+package pro03;
+	//배열 대입
+import java.util.Arrays;
+
+public class Code26_5 {
+
+	public static void main(String[] args) {
+		int array1[] = new int[] {1, 2, 3, 4, 5};
+		int array2[];
+		
+		 
+		array2 = array1;
+		
+		System.out.println("array1 : "+array1);	
+		System.out.println("array2 : "+array2);	//array2 참조변수에 array1의 값을 넣었기 때문에 동일한 참조값이 출력된다.
+		
+		array2[2] = 100;
+		
+		System.out.println("array1 : "+Arrays.toString(array1));
+	 	System.out.println("array2 : "+Arrays.toString(array2));	//array2는 array1의 참조값을 동일하게 갖기때문에, array1의 요소가 변경되면 동일하게 변경된 값이 출력된다.
+	}
+}
