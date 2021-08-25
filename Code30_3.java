b6e604c4106ab701620ab0f3655f17d0bb863a29
kr-Yoon <yoon92_kr@naver.com>
2021-08-25 21:08:23 +0900

    2021.08.25 메소드_3
    매개변수는 없고, 반환값이 있는경우.

---
+++ Code30_3.java
@@ -0,0 +1,19 @@
+package pro03;
+	//매개변수는 없고, 반환값이 있는 경우.
+public class Code30_3 {
+
+	public static int doSomething()
+	{
+		int result;
+		result = 10*100;
+		return result;
+	}
+	
+	public static void main(String[] args) 
+	{
+		int x;
+		x = doSomething();
+		System.out.println("x : "+x);
+		
+	}
+}
