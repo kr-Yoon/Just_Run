903ae04c47ac3294a550bc73b0e9da73102886e0
kr-Yoon <yoon92_kr@naver.com>
2021-08-25 21:02:12 +0900

    2021.08.25 문자열
    String은 문자가 모인 문자열이 아닌, 하나의 메소드이다.

---
+++ Code30_1.java
@@ -0,0 +1,15 @@
+package pro03;
+	//¸Þ¼Òµå
+public class Code30_1 {
+	
+	public static void show() {
+		System.out.println("i am show method");
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("before show call");
+		show();
+		System.out.println("after show call");
+	}
+}
