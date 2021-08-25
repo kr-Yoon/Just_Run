5c086db130e1966281237672ec2b0338530bda0d
kr-Yoon <yoon92_kr@naver.com>
2021-08-25 21:12:39 +0900

    2021.08.25 가변인수
    ▶ 가변인수의 형식
    자료형 ...변수명
    int ...v
    ▶ 가변인수란 여러 변수를 인수로 받는 의미로 배열과는 다른개념이다.

---
+++ Code30_8.java
@@ -0,0 +1,19 @@
+package pro03;
+	//가변인수
+	//
+public class Code30_8 {
+	public static void varArg(int ...v) {
+		
+		System.out.println("numver of arguments :"+v.length);
+		for(int i=0;i<v.length;i++) {
+			System.out.println(v[i]);
+		}
+	}
+	public static void main(String[] args) {
+		varArg();
+		varArg(1);
+		varArg(5,10);
+		varArg(2,3,45);
+		
+	}
+}
