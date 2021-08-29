8261a71a9da498551e615339569b7121af935818
yoon92kr <yoon92_kr@naver.com>
2021-08-29 20:13:31 +0900

    2021.08.24 논리연산자 ▶논리곱, 논리합, 논리부정 연산자로 나뉘며 좌항이 거짓이면 바로 false를 return 하는 논리곱(and)의 처리속도가 논리합보다 빠르기에 우선처리 된다. ▶논리부정은 단항연산에 포함된다.

---
+++ Code017.java
@@ -0,0 +1,12 @@
+package JustRunJava;
+	//논리연산자 A&&B 논리 곱, A||B 논리 합, !A 논리 부정
+
+public class Code017 {
+	public static void main(String[] args) {
+		int a = 10, b = 20, c = 30, d = 15;
+		boolean b1 = a>b && c>d;	// &&는 논리 곱(and)이기 때문에 첫 대소비교 연산자가 거짓이기에 그담은 연산을 진행하지 않고 거짓을 돌려준다.
+		boolean b2 = a!=b || b>c;
+		boolean b3 = a<=b || c<=d;	// ||는 논리 합(or)이기 때문에 첫 연산자의 값이 거짓이어도 다음 연산을 확인한다.
+		System.out.println(b1 + " " +b2+ " " +b3);
+	}
+}
