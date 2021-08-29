fce32b6cc9268ad4068a0846b851971346c88d9e
yoon92kr <yoon92_kr@naver.com>
2021-08-29 22:31:04 +0900

    2021.08.26 static 메소드 ▶필드가 static 키워드일 경우, 이를 활용하는 클래스 메소드 역시 static 이 선언되어야 한다. ▶활용 예제

---
+++ Code067.java
@@ -0,0 +1,25 @@
+package JustRunJava;
+	//static ¸Þ¼Òµå
+class Person{
+	static int count;
+	String name;
+	
+	Person() {
+		count++;
+	}
+	
+	static void printCount() {
+		System.out.println("Person : "+count);
+	}
+}
+public class Code067 {
+	public static void main(String[] args) {
+		Person.printCount();
+//		Person p1 = new Person();
+//		Person p2 = new Person();
+//		Person p3 = new Person();
+		Person.printCount();
+		
+	}
+
+}
