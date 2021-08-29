450e0f5623f385e8780e155e9a883f073023c204
yoon92kr <yoon92_kr@naver.com>
2021-08-29 22:46:07 +0900

    2021.08.27 클래스 상속 ▶클래스 상속의 형식 class 자식클래스명 extends 부모클래스명 ▶자식클래스는 부모클래스의 모든 멤버를 사용할 수 있다.(단 원하는 멤버만 상속받을 수는 없다) ▶특수 메소드 super은 생성자에서 사용되며, 명시적선언을 하지 않을 경우 디폴트선언이 된다. ▶super 메소드 형식 super(부모클래스 생성자 필드명)

---
+++ Code074.java
@@ -0,0 +1,41 @@
+package JustRunJava;
+	//클래스 상속
+class Person12{
+	String name;
+	String phone;
+	
+	Person12(){}
+	Person12(String name, String phone){
+		this.name = name;
+		this.phone = phone;
+	}
+	
+	void printPerson() {
+		System.out.println("name : "+name+" phone : "+phone);
+	}
+}
+
+class Student3 extends Person12{
+	private int score;
+	
+	Student3() {}
+	Student3(String name, String phone, int score){
+		super(name, phone);		//super 메소드는 반드시 자식 생성자 첫 라인에 사용되어야 하며, 선언하지 않아도 자동생성된다.
+		this.score = score;		//Student3 클래스 호출 시 , 부모객채 생성 - 자식객채 생성(부모객채 참조값 보관)
+	}
+	
+	void printStudent() {
+		System.out.println("Student name :"+name+" phone : "+phone+" score : "+score);
+	}
+	
+}
+
+public class Code074 {
+
+	public static void main(String[] args) {
+		Student3 s1 = new Student3("Yoon", "010-0000-0000", 100);
+		
+	s1.printStudent();
+	s1.printPerson();
+	}
+}
