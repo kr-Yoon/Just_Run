1527df851e02e52c9b5d14f805e1beb0404da155
yoon92kr <yoon92_kr@naver.com>
2021-08-29 18:28:28 +0900

    2021.08.23 진법 ▶리터럴 값의 정수타입의 기본형은 10진법으로 적용된다. ▶2진법 사용시 리터럴값 앞에 0b 혹은 0B를 접미사로 붙인다. ▶8진법 사용시 리터럴값 앞에 0을 접미사로 붙인다. ▶16진법 사용시 리터럴값 앞에 0x, 혹은 0X를 붙인다. 단, 10~15을 표현하는 a(A)~f(F)는 접미사를 붙이는 것 x(X)에 따른다.

---
+++ Code011.java
@@ -0,0 +1,18 @@
+package JustRunJava;
+
+public class Code011 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 0b1010;
+		int c = 012;
+		int d = 0xa;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+	}
+
+}
