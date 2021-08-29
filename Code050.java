7998eea2ba49642f1fc17f43460f59ff867cdc6c
yoon92kr <yoon92_kr@naver.com>
2021-08-29 21:54:57 +0900

    2021.08.25 메소드4 ▶매개변수와 반환값이 모두 있는경우. ▶반환값이 있기때문에 void 키워드를 사용하지 않으며, 반환값의 자료형을 메소드에 붙인다.

---
+++ Code050.java
@@ -0,0 +1,26 @@
+package JustRunJava;
+	//매개변수와 반환값이 모두 있는 경우.
+import java.util.Scanner;
+
+public class Code050 {
+	static int add(int x, int y) {
+		int result;
+		result = x+y;
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Scanner typing = new Scanner(System.in);
+		System.out.print("Enter integers : ");
+		int a = typing.nextInt();
+		System.out.print("Enter integers : ");
+		
+		int b = typing.nextInt();
+		int c = add(a,b);
+		System.out.println(a+" + "+b+" = "+c);
+		
+		typing.close();	//Scanner은 물리적 스트링을 받는 함수이기 때문에 종료 메소드를 사용해주어야한다.
+		
+	}
+
+}
