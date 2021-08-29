583d2b0375f4c4e45956b46c7f3880c2e6335030
yoon92kr <yoon92_kr@naver.com>
2021-08-29 21:52:11 +0900

    2021.08.25 메소드 ▶메소드란 행위, 연산을 하는 코드이며 재사용이 가능하다. ▶선언과 처리문이 있어야 정의되었다고 한다.

---
+++ Code047.java
@@ -0,0 +1,15 @@
+package JustRunJava;
+	//¸Þ¼Òµå
+public class Code047 {
+	
+	public static void show() {
+		System.out.println("i am show method");
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("before show call");
+		show();
+		System.out.println("after show call");
+	}
+}
