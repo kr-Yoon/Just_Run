840f05424df18d740ec849b9879534309b24768d
yoon92kr <yoon92_kr@naver.com>
2021-08-29 21:11:18 +0900

    2021.08.24 이항연산자 ▶ 곱하기(*), 몫(/), 나머지(%)가 더하기(+), 빼기(-)보다 연산 우선순위가 높다.

---
+++ Code033.java
@@ -0,0 +1,22 @@
+package JustRunJava;
+	//이항 연산자.
+
+public class Code033 { 
+
+	public static void main(String[] args) {
+		int a = 30, b = 7;
+		int c, d, e, f, g;
+		
+		c = a+b;
+		d = a-b;
+		e = a*b;
+		f = a%b;	//나머지
+		g = a/b;	//몫
+		
+		System.out.println("c: "+c);
+		System.out.println("d: "+d);
+		System.out.println("e: "+e);
+		System.out.println("f: "+f);
+		System.out.println("g: "+g);
+	}
+}
