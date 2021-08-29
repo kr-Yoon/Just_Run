6573591278a30f43afefb3e361f3c8334d142734
yoon92kr <yoon92_kr@naver.com>
2021-08-29 22:26:41 +0900

    2021.08.26 this 키워드 ▶ this 키워드 형식 this.필드명; ▶this 키워드는 객체 자신을 가르친다. ▶변수명이 중복될 때 구분하기 위해 사용된다. ▶생성자에서 title이라는 String 자료형을 인수로 받은 뒤, 처리문의 대입시, 객체 자신의 title 필드에 대입.

---
+++ Code064.java
@@ -0,0 +1,21 @@
+package JustRunJava;
+class Book{
+	String title;
+	int price;
+	
+	Book(String title, int price){
+		this.title = title;
+		this.price = price;
+	}
+	
+}
+public class Code064 {
+
+	public static void main(String[] args) {
+		Book b1 = new Book("ÇÇÅÍÆÒ", 5000);
+		Book b2 = new Book("¾Ë¶óµò", 10000);
+		
+		System.out.println(b1.title+b1.price);
+		System.out.println(b2.title+b2.price);
+	}
+}
