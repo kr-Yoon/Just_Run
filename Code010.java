25ba1b62e5d67c73ae5c6b30b7f6b957e2677b08
yoon92kr <yoon92_kr@naver.com>
2021-08-29 18:25:06 +0900

    2021.08.23 float 자료형 ▶실수 자료형으로 4byte의 메모리를 할당한다. ▶정수자료형(부호, 자료값)과 달리 정밀도라는 속성을 하나 더 갖는다. ▶float자료형은 소수점 6자리까지 표현가능하다. ▶정확한 값이 산출되지 않기도 하는데 이는, 2진법으로 표현가능한 소수가 명확하지 않기 때문이다. ▶실수형의 기본타입은 double 자료형 이기 때문에, 리터럴값에 사용시 f혹은 F를 붙여줘야한다.

---
+++ Code010.java
@@ -0,0 +1,21 @@
+package JustRunJava;
+
+public class Code010 {
+
+	public static void main(String[] args) {
+		float f1 = 5.3F;
+		float f2 = 3.775F;
+		float f3;
+		f3 = f1 + f2;
+		System.out.println(f3);
+		
+		double d1 = 5.3;
+		double d2 = 3.775;
+		double d3;
+		d3 = d1 + d2;
+		System.out.println(d3);
+	
+
+	}
+
+}
