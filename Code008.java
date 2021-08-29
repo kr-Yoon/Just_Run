65612557f92e1e776fbc60d900cbeffae39b4043
yoon92kr <yoon92_kr@naver.com>
2021-08-29 18:22:06 +0900

    2021.08.23 int 자료형 ▶정수자료형의 기본형이며 4byte의 메모리를 할당한다. ▶cpu에서 최적 처리공간인 4byte이며, 가장많이 사용되는 정수자료형이다.

---
+++ Code008.java
@@ -0,0 +1,19 @@
+package JustRunJava;
+
+public class Code008 {
+
+	public static void main(String[] args) {
+		int i = 2147483647;
+		System.out.println(i);
+		i++;
+		System.out.println(i);
+		
+		int j = -2147483648;
+		System.out.println(j);
+		j--;
+		System.out.println(j);
+		
+
+	}
+
+}
