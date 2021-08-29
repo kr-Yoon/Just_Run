fad04f47a387c1214e0661a7a2429f99fd17e2ce
yoon92kr <yoon92_kr@naver.com>
2021-08-29 21:41:40 +0900

    2021.08.25 Arrays 클래스 ▶ 배열에 사용하기 좋은 클래스 ▶Arrays.toString(참조변수명); 을 활용해 참조변수가 가지고있는 자료값을 출력할 수 있다.

---
+++ Code039.java
@@ -0,0 +1,13 @@
+package JustRunJava;
+	//1차배열
+	//배열을 모두 출력하기 위해서는 util.Arrays 메소드를 호출해야한다.
+
+import java.util.Arrays;
+
+public class Code039 {
+	public static void main(String[] args) {
+		int[] score = {90,88,95,82,75,100,85};	//중괄호 앞에 int[] new 가 생략되어 있다.
+		System.out.println(Arrays.toString(score));
+		
+	 }
+}
