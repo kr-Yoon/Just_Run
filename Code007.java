790a811daa723e1e9676c3b819bc6ec8b9cc435d
yoon92kr <yoon92_kr@naver.com>
2021-08-29 18:21:10 +0900

    2021.08.23 short 자료형 ▶ 2byte의 메모리를 할당하는 자료형이다. ▶-32768 ~ 32767의 수를 표현할 수 있다. byte와 마찬가지로 오버된 값에 대해서는 최대값에서 최소값, 최소값에서 최대값으로 return된다.

---
+++ Code007.java
@@ -0,0 +1,23 @@
+package JustRunJava;
+
+public class Code007 {
+
+	public static void main(String[] args) {
+		short s = 32767;
+		System.out.println(s);
+		s++;
+		System.out.println(s);
+		s++;
+		System.out.println(s);
+		
+		short v = -32767;
+		System.out.println(v);
+		v--;
+		System.out.println(v);
+		v--;
+		System.out.println(v);
+	
+
+	}
+
+}
