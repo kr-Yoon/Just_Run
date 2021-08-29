6f4f671f7f831fdaed6c67d399172ea7f686591e
yoon92kr <yoon92_kr@naver.com>
2021-08-29 20:56:01 +0900

    2021.08.24 boolean 연산자 ▶연산의 우선순위를 기억할것

---
+++ Code022.java
@@ -0,0 +1,22 @@
+package JustRunJava;
+	//불리언 논리 연산자
+public class Code022 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 5, c = 20;
+		boolean w, x, y , z;
+		
+		w = a>b;
+		x = a == c;
+		y = (a>b) && (b!=c);
+		z = !(a==b) || (a<b);	//소괄호는 연산우선을 두기 위함
+		
+		
+		System.out.println("w : "+w);
+		System.out.println("x : "+x);
+		System.out.println("y : "+y);
+		System.out.println("z : "+z);
+		
+	}
+
+}
