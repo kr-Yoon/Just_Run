d7c9615648f6f469401616b32a7fc66f4fd8b4ec
yoon92kr <yoon92_kr@naver.com>
2021-08-29 21:16:17 +0900

    2021.08.24 단항연산자2 ▶ 연산,증감,대입연산자의 순서를 잘 기억해야한다.

---
+++ Code036.java
@@ -0,0 +1,39 @@
+package JustRunJava;
+	//단항연산자 추가실습, 선연산/후증가/대입연산자의 순서를 잘 기억해야한다.
+public class Code036 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 10;
+		int z;
+		
+		System.out.println("---------------------");
+		x++;	// x==11
+		++x;	// x==12
+		System.out.println("x= "+x);
+		
+		System.out.println("---------------------");
+		y--;	// y==9
+		--y;	// y==8
+		System.out.println("y= "+y);
+		
+		System.out.println("---------------------");
+		z = x++;	// z==12 x==13
+		System.out.println("z= "+z);
+		System.out.println("x= "+x);
+		
+		System.out.println("---------------------");
+		z = ++x;	// z==14 x==14
+		System.out.println("z= "+z);
+		System.out.println("x= "+x);
+		
+		System.out.println("---------------------");
+		z = ++x + y++;	// z==23 y==9 x==15
+		System.out.println("z= "+z);
+		System.out.println("y= "+y);
+		System.out.println("x= "+x);
+		
+
+	}
+
+}
