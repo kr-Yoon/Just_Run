d5f0265f05aeebef802f2295fad0b3abd92c2653
yoon92kr <yoon92_kr@naver.com>
2021-08-29 15:50:55 +0900

    2021.08.23 print 메소드 형식 System.out.println(출력값) 자바 기본지원 메소드이며, 출력값의 자료형에 무관하게 콘솔을 통해 출력해준다.

---
+++ Code001.java
@@ -0,0 +1,12 @@
+package JustRunJava;
+
+public class Code001 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("World");
+		System.out.println("Java coding is fun");
+
+	}
+
+}
