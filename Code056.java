3011883941c7652a8d613c392fc50052f53ba7b4
yoon92kr <yoon92_kr@naver.com>
2021-08-29 22:06:18 +0900

    2021.08.25 가변인수 응용 ▶가변인수를 응용한 예시이다. ▶가변인수는 여러 매개변수 중 가장 나중에 선언되어야 한다.

---
+++ Code056.java
@@ -0,0 +1,22 @@
+package JustRunJava;
+
+public class Code056 {
+	
+	public static int largerThanA(int A, int ...v) {
+		int sum=0;
+		for(int i=0;i<v.length;i++) {
+			if(v[i] >= A)
+				sum += v[i];
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+				
+		
+		int x = largerThanA(5,50,50,50,50,50,5,05,0,50);
+		
+		System.out.println("larger Than First number is : "+x);
+		
+	}
+}
