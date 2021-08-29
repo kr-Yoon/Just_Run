9008b8495482a5c0cffd9a4afd1719c62b214a12
yoon92kr <yoon92_kr@naver.com>
2021-08-29 21:51:10 +0900

    2021.08.25 문자열 ▶String는 클래스이며, " " 은 앞에 new String가 생략된 것이다.

---
+++ Code046.java
@@ -0,0 +1,15 @@
+package JustRunJava;
+	//문자열
+	//""은 앞에 new String
+public class Code046 {
+
+	public static void main(String[] args) {
+		String s1 = "hello";
+		String s2 = new String("world");
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		
+	}
+}
+ 
