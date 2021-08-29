548434bcd08faefd23b6124256ec20bc46cf21b0
yoon92kr <yoon92_kr@naver.com>
2021-08-29 21:02:27 +0900

    2021.08.24 반복문 while ▶while 반복문 형식 변수 초기화; while(조건) {처리문 + 증감}

---
+++ Code026.java
@@ -0,0 +1,15 @@
+package JustRunJava;
+	//¹Ýº¹¹® while
+public class Code026 {
+	public static void main(String[] args) {
+		int a = 1;
+		int sum = 0;
+		while (a <= 10)
+		{
+			sum += a;
+			a += 1;
+			
+		}
+		System.out.println("sum : "+sum);
+	}
+}
