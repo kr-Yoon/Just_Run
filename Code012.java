7cff48e0e0de6c4bf8b225e80869c2c41de61560
yoon92kr <yoon92_kr@naver.com>
2021-08-29 18:30:31 +0900

    2021.08.23 String 자료형 ▶문자(char)의 집합형태인 문자열이다. ▶String는 클래스이다.

---
+++ Code012.java
@@ -0,0 +1,12 @@
+package JustRunJava;
+
+public class Code012 {
+
+	public static void main(String[] args) {
+		String myName = "Peter Pan";
+		String yourName = "Pig";
+		System.out.println("I am" +myName+ ".");
+		System.out.println("You are " +yourName+".");
+	}
+
+}
