5bcf031f324ca057db5cb494ebb0fd828adb83ba
yoon92kr <yoon92_kr@naver.com>
2021-08-29 22:39:16 +0900

    2021.08.26 자료형 클래스 ▶매개변수 자료형에 선언한 클래스형을 사용할 수 있다.

---
+++ Code071.java
@@ -0,0 +1,39 @@
+package JustRunJava;
+	//매개변수에도 클래스가 들어갈 수 있다.
+class Point{
+	private int x,y;
+	
+	Point(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+	
+	void setX(int x) {
+		this.x = x;
+	}
+	void setY(int y) {
+		this.y = y;
+	}
+	int getX() {
+		return x;
+	}
+	int getY() {
+		return y;
+	}
+	
+	
+}
+public class Code071 {
+	static void change(Point t) {
+		t.setX(20);
+		t.setY(100);
+	}
+	
+	public static void main(String[] args) {
+		Point p = new Point(2,10);
+		System.out.println("befor x : "+p.getX()+" y : "+p.getY());
+		change(p);
+		System.out.println("after x : "+p.getX()+" y : "+p.getY());
+		
+	}
+}
