5c043b9af6743cc6df85ef021707331980df28a4
yoon92kr <yoon92_kr@naver.com>
2021-08-29 20:50:14 +0900

    2021.08.24 연산자 우선순위 ▶산술연산자 > 비교논산자 > 논리연산자 > 대입연산자 순의 우선순위를 갖는다. ▶산술연산자 내에서는 단항연산자가 우선순위가 높다.

---
+++ Code019.java
@@ -0,0 +1,19 @@
+package JustRunJava;
+	//연산자 우선순위_산비논대(산술연산자에서는 단항연산자가 가장 순위가 높다.)
+public class Code019 {
+
+	public static void main(String[] args) {
+		int a = 20, b = 7, c = 30, d = 2, e = 8;
+		int x = a / b + c * d % e;
+		System.out.println("x = "+x);
+		
+		boolean y = a > b + c && d < 8;
+		System.out.println("y = "+y);
+		
+		int z = 100;
+		z += a + b - c * d / e;
+		System.out.println("z = "+z);
+
+	}
+
+}
