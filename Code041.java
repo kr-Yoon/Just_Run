153f4fd3a5213e21630b2af2adea5cab2de32268
yoon92kr <yoon92_kr@naver.com>
2021-08-29 21:44:17 +0900

    2021.08.25 arraycopy 메소드 ▶arraycopy 메소드의 형식 arraycopy(가져올 참조변수명, 시작index, 받을 참조변수명, 시작index, 가져올 length); 가져온 값을 받는 참조변수에 대입한다.

---
+++ Code041.java
@@ -0,0 +1,22 @@
+package JustRunJava;
+import java.util.Arrays;
+	//배열 복사
+public class Code041 {
+
+	public static void main(String[] args) {
+		
+		int[] oldArray = new int[] {1,2,3,4,5,6};
+		int[] newArray = new int[] {10,20,30,40,50,60,70,80,90,100};
+		System.out.println(Arrays.toString(newArray));
+		System.out.println(Arrays.toString(oldArray));
+		
+		System.arraycopy(oldArray, 0, newArray, 4, 5);
+		
+		System.out.println(Arrays.toString(newArray));
+		System.out.println(Arrays.toString(oldArray));
+		
+		 
+		
+		
+	}
+}
