1c40b9550f8a4cf16f1f9134cfd4f2c15a24293f
yoon92kr <yoon92_kr@naver.com>
2021-08-29 21:06:48 +0900

    2021.08.24 continue ▶반복문 시행 중, 중간에 제외하고자 하는 값이 있을때 사용 ▶continue가 실행되면, continue와 해당 반복문, 조건문 등의 끝 중괄호 까지의 코드를 무시한 뒤 조건문으로 돌아간다.

---
+++ Code030.java
@@ -0,0 +1,15 @@
+package JustRunJava;
+	//continue 구문
+	//반복문은 계속 시행하고싶으나, 중간에 빼고자 하는 값이 있을때 사용.
+	//continue가 실행되면, continue와 끝 중괄호 까지의 코드를 무시한채, 조건으로 간다.
+public class Code030 {
+
+	public static void main(String[] args) {
+		int a = 0;
+		while (a<10)
+		{ a++;
+		if (a%3==0) continue;
+		System.out.println(a);
+		}
+	}
+}
