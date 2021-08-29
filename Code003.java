6295c9b6fd2c376f642ee6ccf6ed281300ae102d
yoon92kr <yoon92_kr@naver.com>
2021-08-29 15:52:40 +0900

    2021.08.23 아스키, 유니코드 ▶문자형의 자료는 아스키(유니코드)를 통해 2진법으로 전환되며, 대표적인 것은 외우는게 좋다. ▶a == 97 A ==65 0==48 가==44032

---
+++ Code003.java
@@ -0,0 +1,21 @@
+package JustRunJava;
+
+public class Code003 {
+
+	
+	public static void main (String[] args) {
+		char c1 ='A', c2 = 'a', c3 = 'B', c4 = 'b';
+		System.out.println((int)c1);
+		System.out.println((int)c2);
+		System.out.println((int)c3);
+		System.out.println((int)c4);
+		
+		char ch = '°¡';
+		System.out.println((int)ch);
+		
+		char num = '0';
+		System.out.println((int)num);
+		
+		
+	}
+}
