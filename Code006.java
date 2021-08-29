f416e8f573f8715f102ada073a387cc260d223b4
yoon92kr <yoon92_kr@naver.com>
2021-08-29 18:20:09 +0900

    2021.08.23 byte 자료형 ▶byte 자료형의 메모리할당은 1byte 이다. ▶byte 자료형의 표현가능 범위는 -128~127(0은 양수에 포함)이다. ▶예시와 같이 표현가능범위에 1의 값을 추가대입하게 될경우, 최대값에서 최소값으로, 최소값에서 최대값으로 return 된다.

---
+++ Code006.java
@@ -0,0 +1,24 @@
+package JustRunJava;
+
+public class Code006 {
+
+	
+	public static void main(String[] args) {
+		byte b = 126;
+		System.out.println(b);
+		b++;
+		System.out.println(b);
+		b++;
+		System.out.println(b);
+		
+				
+		byte a = -127;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+			
+		
+	}
+}
