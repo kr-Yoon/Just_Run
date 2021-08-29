4b21c277c7f2c589b971019b2335f596f92dbb42
yoon92kr <yoon92_kr@naver.com>
2021-08-29 22:16:11 +0900

    2021.08.26 클래스5 ▶매개변수가 있는 생성자 선언 시, jvm은 디폴트 생성자를 자동생성해주지 않는다. ▶따라서 디폴트생성자가 필요할 경우 명시적 선언을 해주어야한다.

---
+++ Code061.java
@@ -0,0 +1,27 @@
+package JustRunJava;
+	//매개변수가 있는 생성자, 디폴트 변수를 생성해주어야한다.
+class Friend1{
+	String name;
+	int age;
+	
+	void printFriend() {
+		System.out.println("name : "+name+" age : "+age);
+	}
+	
+	Friend1() {}	//매개변수가 존재하는 생성자를 정의하면, 디폴트 생성자를 자동으로 구현해주지 않는다.
+	Friend1(String a, int b) {
+		name = a;
+		age = b;
+	}
+	
+}
+public class Code061 {
+	public static void main(String [] args) {
+		Friend1 x = new Friend1();
+		x.printFriend();
+		
+		Friend1 y = new Friend1("Yoon", 30);
+		y.printFriend();
+	}
+
+}
