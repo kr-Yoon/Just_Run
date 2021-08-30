a8984ceb24639eab69dade745185af1744477649
yoon92kr <yoon92_kr@naver.com>
2021-08-30 21:35:23 +0900

    2021.08.30 인터페이스5 ▶자바8버전에서는 인터페이스에 default, static 메소드, 필드가 추가되었다. ▶자바9버전에서는 인터페이스에 private 메소드가 추가되었다.

---
+++ Code113.java
@@ -0,0 +1,8 @@
+package JustRunJava;
+
+public interface Code113 {
+	int get();
+	static int getData() {
+		return 100;
+	}
+}
---
+++ Code114.java
@@ -0,0 +1,9 @@
+package JustRunJava;
+
+public class Code114 {
+	
+	public static void main(String[] args) {
+		System.out.println("With Static.getData() : "+Code113.getData());
+		
+	}
+}
