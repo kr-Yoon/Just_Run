a07c505b8faadafcf5418fa20ad8f4c4842a1771
yoon92kr <yoon92_kr@naver.com>
2021-08-30 20:49:23 +0900

    2021.08.30 패키지 ▶패키지는 쉽게 생각하면 폴더의 개념으로 클래스를 종속한다. ▶흔히 사용하던 String, System도 클래스이며 이들은 java.lang 패키지안에 선언된 클래스이다.

---
+++ Code083.java
@@ -0,0 +1,14 @@
+package JustRunJava;
+	//패키지의 개념
+	//String, System도 클래스이며 이들도 java.lang이라는 패키지 안에 선언된 클래스이다.
+
+public class Code083 {
+	public static void main(String[] args) {
+		
+	
+	java.lang.String name = "Yoon";
+	int age = 30;
+	java.lang.System.out.println(name + " is "+age+" years old.");
+	
+	}
+}
