fd6cc9bf4138444a8a9571f1f42b66a2f6f4f5b0
yoon92kr <yoon92_kr@naver.com>
2021-08-31 22:50:34 +0900

    2021.08.31  Random 클래스 ▶임의의 값을 생성할 수 있는 객체를 생성한다. ▶기본값은 0이상 1미만 값을 생성한다. ▶nextint(a) == a의 값 내 정수를 임의 생성한다(기본값에 a를 곱한다고 생각하면 쉽다)

---
+++ Code126.java
@@ -0,0 +1,12 @@
+package JustRunJava;
+	//random Å¬·¡½º
+import java.util.Random;
+public class Code126 {
+	public static void main(String[] args) {
+		Random r1 = new Random();
+		System.out.println(r1);
+		
+		System.out.println(r1.nextInt());
+	}
+
+}
