ef6c3e0d01f3aca4272bbe7ad1fb3fea5cbb4062
yoon92kr <yoon92_kr@naver.com>
2021-08-31 22:47:43 +0900

    2021.08.31 Math 클래스 ▶생성자가 없으며, 두 개의 상수(double E //자연 로그 e값, double PI //원주율)와 메소드로만 구성되어있다.

---
+++ Code125.java
@@ -0,0 +1,17 @@
+package JustRunJava;
+	//java.lang.Math 클래스
+	//반환값을 항시 확인할 것
+public class Code125 {
+	
+public static void main(String[] args) {
+	System.out.println("1."+Math.abs(-.35));	//절대값
+	System.out.println("2. "+Math.ceil(-3.5));	//올림
+	System.out.println("3. "+Math.floor(-3.5));	//내림
+	System.out.println("4. "+Math.max(2.7, 6.7));	//큰값 반환
+	System.out.println("5. "+Math.min(2.7, 6.1));	//작은값 반환
+	System.out.println("6. "+Math.pow(2, 10));	//a의 b 제곱
+	System.out.println("7. "+Math.round(3.7));	//반올림
+	System.out.println("8. "+Math.sqrt(25));		//루트값 반환
+	
+}
+}
