b08f9d9fb7808a7b943bf14c7a3a97dbc27b3372
yoon92kr <yoon92_kr@naver.com>
2021-08-31 21:19:41 +0900

    2021.08.31 Object ▶ Object 클래스는 모든 클래스에 자동 상속된다.

---
+++ Code119.java
@@ -0,0 +1,16 @@
+package JustRunJava;
+import java.util.Date;
+
+public class Code119 {
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Date obj2 = new Date();
+		
+		System.out.println(obj1.toString());
+		System.out.println(obj2.toString());
+		
+		int[] obj3 = {1,2,3,4,5};
+		System.out.println(obj3.toString());
+	}
+
+}
