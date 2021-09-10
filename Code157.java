bd93e581aa0b62493ae6dd93b471687f714a06bb
yoon92kr <yoon92_kr@naver.com>
2021-09-10 17:26:51 +0900

    2021.09.10 main 메소드 이해 ▶ 메인스레드가 작동키로 약속된 메소드 ▶ 매개변수인 args에 값을 입력하게 되면, 해당 값을 반환해준다.

---
+++ Code157.java
@@ -0,0 +1,12 @@
+package JustRunJava;
+// main 메소드의 이해
+// main 메소드의 매개변수 String[] args는 java를 실행할 때 인자로 받게되고, 같이 호출되게 된다.
+// 프롬포트에서 java Code157 a b c 를 매개변수로 실행시키면 Hello World a b c 가 출력된다.
+
+public class Code157 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+}
