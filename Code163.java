e37c0066b351102ddf64267f75265dc191609b43
yoon92kr <yoon92_kr@naver.com>
2021-09-14 23:46:12 +0900

    2021.09.14 알고리즘 실습_1 ▶ KeyWorld : while, flag, ArrayList, for-each ▶ 문제해결은 완료하였으나 첫 input값을 정상적으로 주면 flag 가 true 된 상태로 프로그램이 다시 시행됨 ▶ 두번째 input에 틀린값을 주어도 "일치하는 학생이 없습니다" 프린트문이 시행하지 않는 예상밖의 처리 ▶ 해결방안이 떠오르지 않아 이후 수정할것

---
+++ Code163.java
@@ -0,0 +1,79 @@
+package JustRunJava;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class StudentName {
+	String name;
+	int no;
+	StudentName(String name, int no){
+		this.name = name;
+		this.no = no;
+	}
+	
+	String getName() {
+		return this.name; 
+	}
+	
+	int getNo() {
+		return this.no; 
+	}
+		
+}
+public class Code163 {
+	
+	public static void main(String[] args) {
+		
+		StudentName s1 = new StudentName("윤상현", 1001);
+		StudentName s2 = new StudentName("안유미", 1002);
+		StudentName s3 = new StudentName("윤상혁", 1003);
+		StudentName s4 = new StudentName("공미옥", 1004);
+		
+		ArrayList<StudentName> al = new ArrayList<StudentName>();
+		al.add(s1);
+		al.add(s2);
+		al.add(s3);
+		al.add(s4);
+		
+		String allow;
+		String quest;
+		boolean flag = false;
+		
+		Scanner text = new Scanner(System.in);
+		
+		while(true) {
+			
+			System.out.println("계속 검색 하시겠습니까? (y/n)");
+			allow = text.next();
+			
+			if (allow.equals("y")){
+				System.out.println("학생 이름을 입력해 주세요 : ");
+				quest = text.next();
+				
+				for (StudentName i:al) {
+					if (i.getName().equals(quest)) {
+						System.out.println(i.getName()+"학생의 학번은 "+i.getNo()+"입니다");
+						flag = true;
+						
+						}
+					}
+				
+				if (!flag) {
+					System.out.println("일치하는 학생이 없습니다.");
+					continue;
+				}
+				else 
+					continue;
+			
+			}
+			
+			
+			else
+				System.out.println("프로그램을 종료합니다...");
+				text.close();
+				break;
+					
+		}
+	}
+}
+
