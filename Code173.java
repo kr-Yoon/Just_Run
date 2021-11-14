e9de0d1af1a50497fe9ebde88f98877962513d8c
yoon92kr <73813647+yoon92kr@users.noreply.github.com>
2021-11-15 08:34:41 +0900

    2021.11.15 다형성2
    ▶ 상속의 개념을 잘 이해해야한다.

---
+++ Code173.java
@@ -0,0 +1,24 @@
+package JustRunJava;
+
+class Parent {
+	Parent() {
+		System.out.println("부모 클래스");
+	}
+}
+
+public class Code173 extends Parent {
+	
+	Code173(){
+		System.out.println("자식 클래스");
+	}
+	
+	public static void main(String[] args) {
+		
+		Parent pa = new Parent();	//허용
+		Code173 ch = new Code173();	//허용
+		Parent pc = new Code173();	//허용
+		Code173 cp = new Parent();	// 오류발생
+		
+	}
+
+}
